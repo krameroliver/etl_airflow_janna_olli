9b3bbd42e8bda39ae6aa61b39e26afc09f1d8ef4
Janna <alafleo@gmx.de>
2021-07-04 21:29:18 +0200

    trans überprüfung in test

--- project/dags/biz_beladung/konto.py
+++ project/dags/biz_beladung/konto.py
@@ -28,6 +28,7 @@ class Ko:
         self.target = 'konto'
         self.schema_src = 'src'
         self.src_acct = 'acct'
+        self.src_trans = 'trans'
         if os.path.isdir(r'/Configs/ENB/'):
             self.conf_r = r'/Configs/ENB/'
         else:
@@ -37,10 +38,27 @@ class Ko:
     def join(self):
         account = read_raw_sql_sat(db_con=connect_to_db(layer=self.schema_src), date=self.date, schema=self.schema_src,
                                   t_name=self.src_acct)
+        trans = read_raw_sql_sat(db_con=connect_to_db(layer=self.schema_src), date=self.date, schema=self.schema_src,
+                                  t_name=self.src_trans)
+        trans=trans[['account_id','balance','fulldatewithtime']]
 
-        data = account
+        trans['fulldatewithtime']=trans['fulldatewithtime'].apply(lambda x: self.timestamp(x))
 
-        return data
+        max_ids=trans.groupby(['account_id'])['fulldatewithtime'].transform(max)==trans['fulldatewithtime']
+        trans=trans[max_ids]
+
+        print(trans.head)
+
+
+        #data = account.merge(trans,  how='inner', left_on=['account_id'], right_on=['account_id'])
+
+        #return data
+
+    def timestamp(self, ts):
+        ts=ts.replace('T',' ')
+        element = datetime.strptime(ts, "%Y-%m-%d %H:%M:%S")
+        timestamp = datetime.timestamp(element)
+        return(timestamp)
 
     def lkp_frequenz(self, freq: str):
         frequenz = {
@@ -56,6 +74,7 @@ class Ko:
         out_data['kontonummer'] = data['account_id']
         out_data['frequenz'] = data['frequency'].apply(lambda x: self.lkp_frequenz(x))
         out_data['wertstellungstag'] = data['parseddate']
+        out_data['kontostand']=data['balance']
 
         return out_data
 
@@ -78,4 +97,11 @@ class Ko:
         dv_hub.load
 
         print('--- Beladung Ende ---\n')
+
+
+konto=Ko('2018-12-31')
+join=konto.join()
+#map=konto.mapping(join)
+print(join)
+
 
--- project/dags/biz_beladung/test.py
+++ project/dags/biz_beladung/test.py
@@ -1,6 +1,16 @@
 import yaml
-target = 'darlehen'
+import pandas as pd
+import numpy as np
+import datetime
+target = 'trans'
 with open(r'../Configs/ENB/' + target + '.yaml') as file:
     documents = yaml.full_load(file)
-target_fields = documents[target]['tables']['s_darlehen']['fields']
-print(target_fields)
+target_fields = documents[target]['tables']['trans']['fields']
+data_types=documents[target]['tables']['trans']['data_types']
+types={target_fields[i]:data_types[i] for i in range(len(target_fields))}
+print(data_types)
+
+data=pd.read_csv(r'../../rawdata/ENB/2018-12-31/trans.csv', delimiter=';',header=0, dtype=types,parse_dates=['fulldate', 'fulltime', 'fulldatewithtime'])
+
+print(data.dtypes)
+
