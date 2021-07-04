946b603efcceca1f2a2b63b6ff1ba1f0248fc7a3
Oliver <krameroliver1@gmail.com>
2021-07-04 23:08:02 +0200

    konto-> cc link

--- project/dags/biz_beladung/test.py
+++ project/dags/biz_beladung/test.py
@@ -1,16 +1,30 @@
 import yaml
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import time
+
 target = 'trans'
 with open(r'../Configs/ENB/' + target + '.yaml') as file:
     documents = yaml.full_load(file)
 target_fields = documents[target]['tables']['trans']['fields']
 data_types=documents[target]['tables']['trans']['data_types']
-types={target_fields[i]:data_types[i] for i in range(len(target_fields))}
-print(data_types)
+rel_types = {}
+parse_list = []
+for k,i in enumerate(target_fields):
+    if data_types[k] != 'DATUM':
+        rel_types[i] = data_types[k]
+    else:
+        rel_types[i] = 'str'
+        parse_list.append(i)
 
-data=pd.read_csv(r'../../rawdata/ENB/2018-12-31/trans.csv', delimiter=';',header=0, dtype=types,parse_dates=['fulldate', 'fulltime', 'fulldatewithtime'])
+print(rel_types)
+print(parse_list)
 
-print(data.dtypes)
+
+
+data=pd.read_csv(r'../../rawdata/ENB/2018-12-31/trans.csv', delimiter=',',header=0, dtype=rel_types,parse_dates=parse_list,infer_datetime_format=True)
+#data['date'] = pd.to_datetime(data['date'],infer_datetime_format='%Y-%m-%d')
+
+
+print(data[['fulldate', 'fulltime', 'fulldatewithtime', 'date']])
 
